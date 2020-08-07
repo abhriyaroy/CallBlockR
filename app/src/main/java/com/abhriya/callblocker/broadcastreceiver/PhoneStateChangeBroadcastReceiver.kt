1114ea46a54f983e4f18117622d880a9d42c6a00
Abhriya <roy.abhriya@gmail.com>
2020-08-08 01:45:01 +0530

    Open main activity on notification click

--- app/src/main/java/com/abhriya/callblocker/broadcastreceiver/PhoneStateChangeBroadcastReceiver.kt
+++ app/src/main/java/com/abhriya/callblocker/broadcastreceiver/PhoneStateChangeBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package com.abhriya.callblocker.broadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.abhriya.callblocker.ui.MainActivity
 import com.abhriya.phonereceiver.PhoneReceiver
 import dagger.android.AndroidInjection
 import javax.inject.Inject
@@ -14,6 +15,6 @@ class PhoneStateChangeBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         AndroidInjection.inject(this, context)
-        phoneReceiver.onPhoneStateChange(context, intent)
+        phoneReceiver.onPhoneStateChange(context, intent, Intent(context, MainActivity::class.java))
     }
 }
--- phonereceiver/src/main/java/com/abhriya/phonereceiver/PhoneReceiver.kt
+++ phonereceiver/src/main/java/com/abhriya/phonereceiver/PhoneReceiver.kt
@@ -1,6 +1,10 @@
 package com.abhriya.phonereceiver
 
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -9,6 +13,8 @@ import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.NonNull
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.abhriya.database.DatabaseHelper
 import com.android.internal.telephony.ITelephony
 import kotlinx.coroutines.GlobalScope
@@ -17,15 +23,14 @@ import kotlinx.coroutines.launch
 class PhoneReceiver(private val databaseHelper: DatabaseHelper) {
 
 
-    fun onPhoneStateChange(context: Context, intent: Intent) {
-
+    fun onPhoneStateChange(context: Context, intent: Intent, onNotificationClickIntent: Intent) {
         println("received here call")
         GlobalScope.launch {
             if (TelephonyManager.ACTION_PHONE_STATE_CHANGED == intent.action && intent.getStringExtra(
                     TelephonyManager.EXTRA_STATE
                 ) == TelephonyManager.EXTRA_STATE_RINGING && shouldRejectPhoneCall(intent)
             ) {
-                rejectCall(context, null)
+                rejectCall(context, null, onNotificationClickIntent)
             }
             if (intent.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_OFFHOOK
             ) {
@@ -52,7 +57,8 @@ class PhoneReceiver(private val databaseHelper: DatabaseHelper) {
     @SuppressLint("MissingPermission")
     private fun rejectCall(
         @NonNull context: Context,
-        number: Number?
+        number: Number?,
+        onNotificationClickIntent: Intent
     ) {
         if (!ALREADY_ON_CALL) {
             var failed = false
@@ -92,42 +98,42 @@ class PhoneReceiver(private val databaseHelper: DatabaseHelper) {
                 ).show()
             }
         }
-//        if (Build.VERSION.SDK_INT >= 26) {
-//            val notificationManager =
-//                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//            val channel = NotificationChannel(
-//                "default",
-//                "Call blocker",
-//                NotificationManager.IMPORTANCE_DEFAULT
-//            )
-//            channel.description = "call blocked"
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//        val notify: Notification = NotificationCompat.Builder(context, "M_CH_ID")
-//            .setContentTitle("title")
-//            .setContentText(
-//                "rejected number man"
-//            )
-//            .setPriority(NotificationCompat.PRIORITY_HIGH)
-//            .setCategory(NotificationCompat.CATEGORY_CALL)
-//            .setShowWhen(true)
-//            .setAutoCancel(true)
-////                .setContentIntent(
-////                    PendingIntent.getActivity(
-////                        context,
-////                        0,
-////                        Intent(context, BlacklistActivity::class.java),
-////                        PendingIntent.FLAG_UPDATE_CURRENT
-////                    )
-////                )
-//            .addPerson("tel:$number")
-//            .setGroup("rejected")
-//            .setChannelId("default")
-//            .setGroupSummary(true) /* swy: fix notifications not appearing on kitkat: https://stackoverflow.com/a/37070917/674685 */
-//            .build()
-////            val tag = if (number != null) number.number else "private"
-//        NotificationManagerCompat.from(context)
-//            .notify("private", NOTIFY_REJECTED, notify)
+        if (Build.VERSION.SDK_INT >= 26) {
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                "default",
+                "Call blocker",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "call blocked"
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notify: Notification = NotificationCompat.Builder(context, "M_CH_ID")
+            .setSmallIcon(R.drawable.ic_launcher_small)
+            .setContentTitle("title")
+            .setContentText(
+                "rejected number man"
+            )
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_CALL)
+            .setShowWhen(true)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    onNotificationClickIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            )
+            .addPerson("tel:$number")
+            .setGroup("rejected")
+            .setChannelId("default")
+            .setGroupSummary(true) /* swy: fix notifications not appearing on kitkat: https://stackoverflow.com/a/37070917/674685 */
+            .build()
+//            val tag = if (number != null) number.number else "private"
+        NotificationManagerCompat.from(context)
+            .notify("private", NOTIFY_REJECTED, notify)
     }
 
     companion object {
