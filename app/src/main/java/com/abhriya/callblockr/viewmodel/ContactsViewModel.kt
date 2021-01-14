e32f7967922bf937dc58ee3b762ede1d3af05d71
Abhriya <abhriya.roy@biofourmis.com>
2021-01-14 13:00:31 +0530

    Fix for release to play store

--- app/src/main/java/com/abhriya/callblockr/viewmodel/ContactsViewModel.kt
+++ app/src/main/java/com/abhriya/callblockr/viewmodel/ContactsViewModel.kt
@@ -124,17 +124,8 @@ class ContactsViewModel @ViewModelInject constructor(private val contactsUseCase
                         })
                         list
                     }.also {
-                        val here = mutableListOf<ContactModel>(
-                            ContactModel("Donal Trump", "+1 889-290-029", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Cristian Ronaldo", "+2848940183", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Joe Bide", "+1 889-290-029", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Justin Trude", "+2848940183", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Lionel Mess", "+1 889-290-029", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Mamata Banner", "+91 11 0202 011", ContactModelType.ALL_CONTACT, false),
-                            ContactModel("Narendra Mod", "+91 11 0202 020 ", ContactModelType.ALL_CONTACT, false),
-                        )
-                        allContactsList = here
-                        _allContactsToShowLiveData.postValue(ResourceState.success(here))
+                        allContactsList = it
+                        _allContactsToShowLiveData.postValue(ResourceState.success(it))
                     }
             } catch (dataLayerException: DataLayerException) {
                 _allContactsToShowLiveData.postValue(
@@ -146,17 +137,7 @@ class ContactsViewModel @ViewModelInject constructor(private val contactsUseCase
 
     fun getCallLog() {
         viewModelScope.launch(Dispatchers.IO) {
-            val here = mutableListOf<CallLogModel>(
-                CallLogModel("Donal Trump", "+1 889-290-029", CallType.REJECTED_CALL, "1610205717","0", false),
-                CallLogModel("Cristian Ronaldo", "+2848940183", CallType.OUTGOING_CALL, "1610205417","160", false),
-                CallLogModel("Joe Bide", "+1 889-290-029", CallType.OUTGOING_CALL, "1610203717","10", false),
-                CallLogModel("Justin Trude", "+1 889-290-029", CallType.MISSED_CALL, "1610200717","0", false),
-                CallLogModel("Lionel Mess", "+1 889-290-029", CallType.INCOMING_CALL, "1610195717","160", false),
-                CallLogModel("Mamata Banner", "+91 11 0202 011", CallType.INCOMING_CALL, "1610194717","120", false),
-                CallLogModel("Narendra Mod", "+91 11 0202 011", CallType.INCOMING_CALL, "1610193617","132", false),
-            )
-//            _callLogList.postValue(contactsUseCase.getCallLog())
-            _callLogList.postValue(here)
+            _callLogList.postValue(contactsUseCase.getCallLog())
         }
     }
 
