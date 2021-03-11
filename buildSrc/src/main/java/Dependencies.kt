4139d190c2e50f1148220b7296f2439d99f69a86
Abhriya <abhriya.roy@biofourmis.com>
2021-03-11 18:10:52 +0530

    Update versionCode to 9

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ object Apps {
     const val buildToolsVersion = "30.0.2"
     const val minSdk = 21
     const val targetSdk = 29
-    const val versionCode = 8
+    const val versionCode = 9
     const val versionName = "1.0.0"
 }
 
